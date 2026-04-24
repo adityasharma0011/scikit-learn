d5bac728cc644be3013dd6d61b2503dc8bf6f735
Aditya Sharma <sharmaaditya0049@gmail.com>
2026-04-25 00:02:06 +0530

    add sample data and rename filess

--- 1-train_test.py
+++ 1-train_test.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 X = [[2], [4], [6], [8]]
 y = [0, 0, 1, 1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.75)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20 , random_state=42)
 
 print("X_train:", X_train)
 print("X_test:", X_test)
--- 4-encoding.py
+++ 4-encoding.py
@@ -1,8 +1,33 @@
 from sklearn.preprocessing import LabelEncoder
+import pandas as pd
 
-gender = ["Male", "Female", "Male"]
+# gender = ["Male", "Female", "Male"]
+# # encoder = LabelEncoder()
+# # encoded = encoder.fit_transform(gender)
+# # print(encoded)
 
-encoder = LabelEncoder()
-encoded = encoder.fit_transform(gender)
 
-print(encoded)
+
+df = pd.read_csv("sample_data.csv")
+df_label = df.copy()
+
+le = LabelEncoder()
+df_label['Gender_Encoded'] = le.fit_transform(df_label['Gender'])
+df_label['Passed_Encoded'] = le.fit_transform(df_label['Passed'])
+
+print('\nlabel encoded data')
+print(df_label[['Name','Gender', 'Gender_Encoded','Passed','Passed_Encoded' ]])
+
+#  above this was all about label encoding, now we will see one hot encoding
+
+
+
+
+df_encoded = pd.get_dummies(df, columns=['City'])
+print('\none-hot encoded data(City)')
+print(df_encoded)
+
+# this is converting the 'City' column into bollean ans task  is to convert into  binary columns
+
+
+
